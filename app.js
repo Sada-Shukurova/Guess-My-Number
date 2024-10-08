a480ee56f24e52664791b2fdb5dd6bb9e2701862
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2024-10-09 01:25:25 +0400

    update js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 'use strict';
 
-// selecting body
+
 const body = document.querySelector( 'body' );
 
-// getting html elements⬇
 const checkBtn = document.querySelector( '.check' );
 const guess = document.querySelector( '.guess' );
 const message = document.querySelector( '.message' );
@@ -14,43 +13,40 @@ const highscore = document.querySelector( '.highscore' );
 const glow = document.querySelector( '.glow' );
 const heading = document.querySelector( 'h1' );
 
-// defining secret number⬇
+//  secret number⬇
 
 let secretNumber = Math.trunc( Math.random() * 20 ) + 1;
 
-// console.log( secretNumber );
 
-// defining initial score
 let scoreValue = 20;
 
-// defining initial highscore value
+
 let highscoreValue = 0;
-// set autofocus property
+
 guess.focus();
 
 
-// Add this variable to keep track of typing animation
 let typingInProgress = false;
 
-//function to set message text content
+
 function displayMessage ( ourMessage ) {
 
-    // message.textContent = ourMessage;
+   
     if ( typingInProgress ) {
-        // If typing is in progress, clear it and display the message immediately
+       
         clearTimeout( typeWriter );
         message.textContent = ourMessage;
         typingInProgress = false;
     } else {
-        // If typing is not in progress, start the animation
+   
         typeWriter( ourMessage );
     }
 
 }
 
-// Update the typeWriter function to accept a message parameter
+
 function typeWriter ( newMessage ) {
-    typingInProgress = true; // Set typing in progress
+    typingInProgress = true;
 
     let i = 0;
     const speed = 80;
@@ -61,12 +57,12 @@ function typeWriter ( newMessage ) {
             i++;
             setTimeout( type, speed );
         } else {
-            // Reset i to 0 to start the typing animation again
+          
             i = 0;
             setTimeout( () => {
-                message.textContent = ''; // Clear the content
-                typingInProgress = false; // Typing animation is complete
-            }, 1000 ); // Wait for 1 second before clearing the message
+                message.textContent = ''; 
+                typingInProgress = false; 
+            }, 1000 ); 
         }
     }
 
@@ -75,46 +71,42 @@ function typeWriter ( newMessage ) {
 
 
 
-// DOM manipulation⬇️
-// function for manipulating element values and their styles
+// DOM 
 function gameLogic () {
     let guessValue = +guess.value;
     // when there is no input
     if ( !guessValue ) {
-        // message.textContent = '⛔ No number!';
+
         displayMessage( '⛔ No number!' );
     } else if ( guessValue === secretNumber ) {
-        // when player wins
-        // message.textContent = '🎉 Correct number!';
-        // displayMessage( '🎉 Correct number!' );
-        // Function to start the fireworks animation
+
         function startFireworksAnimation () {
             ( async () => {
-                // Wait for the tsParticles library to be loaded (only if you are not using the bundle script)
+                
                 await loadFireworksPreset( tsParticles );
 
-                // Configure and start the tsParticles animation inside the "fireworks-container"
+            
                 tsParticles.load( "fireworks-container", {
                     preset: "fireworks",
                 } );
             } )();
         }
 
-        // Function to stop the fireworks animation and return to the original page
+     
         function stopFireworksAnimation () {
-            // Remove the "fireworks-container" from the DOM
+       
             const fireworksContainer = document.getElementById( "fireworks-container" );
             fireworksContainer.parentNode.removeChild( fireworksContainer );
             message.innerHTML = '<i class="fa-solid fa-star fa-beat"></i> You won!';
         }
 
-        // Call the startFireworksAnimation function to begin the animation
+      
         startFireworksAnimation();
 
-        // After 2 seconds, stop the fireworks animation and return to the original page
+     
         setTimeout( () => {
             stopFireworksAnimation();
-            // Add any code here to perform actions after the fireworks animation
+           
         }, 4000 );
 
         body.style.overflow = 'hidden';
@@ -132,21 +124,18 @@ function gameLogic () {
         // when guess is wrong
     } else if ( guessValue !== secretNumber ) {
         if ( scoreValue > 1 ) {
-            // when guess is higher or lower than secret number
-            // message.textContent = guessValue > secretNumber ? '📈Too high' : '📉 Too low';
+
             displayMessage( guessValue > secretNumber ? '⬆️Too high, Try less' : '⬇️ Too low, Try high' );
             scoreValue--;
             score.textContent = scoreValue;
             guess.focus();
             guess.value = '';
         } else {
-            // check if score is 0 and player lost
-            // message.textContent = '💥 You lost the game!';
-            // displayMessage( '💥 You lost the game!' );
+
             message.innerHTML = '<i class="fa-solid fa-bomb fa-beat-fade"></i>  You lost the game!';
             score.textContent = 0;
             guess.setAttribute( 'disabled', '' );
-            checkBtn.disabled = true; // Disable the "Check!" button
+            checkBtn.disabled = true; /
             body.classList.add( 'loser' );
             guess.style.color = "#fff";
             message.style.color = "#fff";
@@ -155,7 +144,7 @@ function gameLogic () {
     }
 };
 guess.addEventListener( "keypress", function ( event ) {
-    // If the user presses the "Enter" key on the keyboard
+ 
     if ( event.key === "Enter" ) {
         event.preventDefault();
         gameLogic();
@@ -163,12 +152,12 @@ guess.addEventListener( "keypress", function ( event ) {
 } );
 
 checkBtn.addEventListener( "click", function () {
-    // Call the same function when the button is clicked
+
     gameLogic();
 } );
 
 
-// locic to play again button- reset all values except highscore value
+// locic 
 againBtn.addEventListener( 'click', function () {
     body.style.overflow = 'visible';
     body.classList.remove( 'loser' );
@@ -192,6 +181,6 @@ againBtn.addEventListener( 'click', function () {
     // console.log( secretNumber );
 } );
 
-// Call typeWriter to start the initial typing animation
+
 typeWriter( "  G u e s s i n g . . .  " );
 
